4d20a5dafb74fab8ff998211a92bc269984e409e
Jyothidk <jyothidk890@gmail.com>
2024-10-15 14:55:13 +0200

    add

--- Project_3/ebs_volume_type_check.py
+++ Project_3/ebs_volume_type_check.py
@@ -7,8 +7,9 @@ def get_volume_id_from_arn(volume_arn):
     
     # split the arn using the colon (':') separator
     arn_parts = volume_arn.split(':')
-    
+
     # The volume id is the last part of the arn after the 'volume/' prefix
+    # Here [-1] indicates last element in the list
     volume_id = arn_parts[-1].split('/')[-1]
     return volume_id
 
@@ -22,4 +23,13 @@ def lambda_handler(event, context):
         VolumeType='gp3',
     )
         
+# def get_volume_id_from_arn(volume_arn):
     
+#     # Split the arn using the '/' separator to get the volume id
+#     arn_parts = volume_arn.split('/')
+    
+#     # output will be like ['arn:aws:ec2:region:account-id:volume', 'vol-0abcd1234efgh5678']
+#     # Here [-1] indicates last element in the list
+#     volume_id = arn_parts[-1]
+#     return volume_id 
+
